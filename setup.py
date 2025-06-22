29bb462e0716360bcae3745846afb2a0cab6eb92
MDMAinsley <lewis.james.short@gmail.com>
2025-06-22 11:44:58 +0100

    beta0.42

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stealth_selenium',
-    version='0.41',
+    version='0.42',
     packages=find_packages(),
     install_requires=[
         'undetected-chromedriver',
